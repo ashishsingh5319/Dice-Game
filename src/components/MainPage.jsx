5cb3302b22cc56f120221e4a1892fbb6acb67a5a
ashishsingh5319 <singhashish5319@gmail.com>
2024-04-24 17:59:34 +0530

    images_update

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import logo from '../images/cube.png'
+import { cube } from '../images/image'
+ 
 const MainPage = ({toggle}) => {
   return (
     <div className="container">
         <div>
-<<<<<<< HEAD
-      <img src={logo} alt="cube" />
+      <img src={cube} alt="cube" />
       </div>
-=======
-      <img src={logo} alt="dice" /></div>
->>>>>>> dabfa147fb0db90bdbe1e7b918f7eb9fbcaa7f37
       
       <div className='content'>
       <h1>DICE GAME</h1>
